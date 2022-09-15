e495a166b46f145d313189734d97448eaaf71e66
zhoujianping <zhoujianping@52tt.com>
2022-09-15 11:37:05 +0800

    正确获取状态栏高度

--- Example/JPCrop/Extension.swift
+++ Example/JPCrop/Extension.swift
@@ -60,12 +60,64 @@ let LandscapeScreenBounds: CGRect = CGRect(origin: .zero, size: LandscapeScreenS
 
 let IsBangsScreen: Bool = PortraitScreenHeight > 736.0
 
+private var _DiffTabBarH: CGFloat = 0
+var DiffTabBarH: CGFloat {
+    guard _DiffTabBarH == 0 else { return _DiffTabBarH }
+    
+    if #available(iOS 11.0, *),
+       let window = UIApplication.shared.delegate?.window ?? UIApplication.shared.windows.first
+    {
+        _DiffTabBarH = window.safeAreaInsets.bottom
+    }
+    
+    guard _DiffTabBarH == 0 else { return _DiffTabBarH }
+    
+    if IsBangsScreen {
+        return 34.0
+    } else {
+        return 0
+    }
+}
 let BaseTabBarH: CGFloat = 49.0
-let TabBarH: CGFloat = IsBangsScreen ? 83.0 : BaseTabBarH
-let DiffTabBarH: CGFloat = TabBarH - BaseTabBarH
+let TabBarH: CGFloat = BaseTabBarH + DiffTabBarH
 
+private var _StatusBarH: CGFloat = 0
+var StatusBarH: CGFloat {
+    guard _StatusBarH == 0 else { return _StatusBarH }
+    
+    if #available(iOS 11.0, *) {
+        if let window = UIApplication.shared.delegate?.window ?? UIApplication.shared.windows.first {
+            if #available(iOS 13.0, *) {
+                if let statusBarManager = window.windowScene?.statusBarManager {
+                    _StatusBarH = statusBarManager.statusBarFrame.height
+                } else {
+                    _StatusBarH = window.safeAreaInsets.top
+                }
+            } else {
+                _StatusBarH = window.safeAreaInsets.top
+            }
+        } else {
+            if #available(iOS 13.0, *) {} else {
+                _StatusBarH = UIApplication.shared.statusBarFrame.height
+            }
+        }
+    } else {
+        _StatusBarH = UIApplication.shared.statusBarFrame.height
+    }
+    
+    guard _StatusBarH == 0 else { return _StatusBarH }
+    
+    if IsBangsScreen {
+        if #available(iOS 13.0, *) {
+            return 48.0
+        } else {
+            return 44.0
+        }
+    } else {
+        return BaseStatusBarH
+    }
+}
 let BaseStatusBarH: CGFloat = 20.0
-let StatusBarH: CGFloat = IsBangsScreen ? 44.0 : BaseStatusBarH
 let DiffStatusBarH: CGFloat = StatusBarH - BaseStatusBarH
 
 let NavBarH: CGFloat = 44.0
